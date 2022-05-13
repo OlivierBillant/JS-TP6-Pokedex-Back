e315b3df4cf04efafcd3bf4c3e06654ae8795b11
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-05-13 16:52:09 +0200

    FINAL

--- src/main/java/fr/eni/ecole/pokedex/bll/bo/Pokemon.java
+++ src/main/java/fr/eni/ecole/pokedex/bll/bo/Pokemon.java
@@ -2,60 +2,16 @@ package fr.eni.ecole.pokedex.bll.bo;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Pokemon {
-	private String name;
 	private int id;
+	private String name;
 	private String image;
 	private List<Type> types;
-	
-	public Pokemon() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	public Pokemon(int id, String name, String image, List<Type> types) {
-		super();
-		this.name = name;
-		this.id = id;
-		this.image = image;
-		this.types = types;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getImage() {
-		return image;
-	}
-
-	public void setImage(String image) {
-		this.image = image;
-	}
-
-	public List<Type> getTypes() {
-		return types;
-	}
-
-	public void setTypes(List<Type> types) {
-		this.types = types;
-	}
-
-	@Override
-	public String toString() {
-		return "Pokemon : " + name + " " + id + " " + image + " " + types;
-	}
-	
-	
 }
