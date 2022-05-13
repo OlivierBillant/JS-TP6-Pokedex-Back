6b9ff90be6145f3f46722c77cfbd41072581cf12
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-05-13 11:47:17 +0200

    CREATE branch main

---
+++ src/main/java/fr/eni/ecole/pokedex/PokedexApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.ecole.pokedex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokedexApplication.class, args);
+	}
+
+}
